3cf52cae40e952f9e4e03a34d7ba52a9d713b8cd
Manuel <mgordillo11@outlook.com>
2022-02-16 21:28:49 -0700

    Fixed up home page and started implementing javascript

---
+++ javascript/Home-JS.js
@@ -0,0 +1,28 @@
+let mySong = document.getElementById("homeSong");
+let icon = document.getElementById("icon");
+
+icon.onclick = async function() {
+
+    const image2 = async function image() {
+        return await fetch('https://picsum.photos/2000/1000')
+    }
+    /*const inspritationalQuote = async function quote() {
+        return await fetch('https://inspiration.goprogram.ai')
+    }*/
+    
+    const image3 = await image2()
+    //const insQuote = await inspritationalQuote()
+
+
+    const updatedImage = image3.url
+    document.getElementById("song-pic").src = updatedImage
+
+    console.log(image3.url)
+    if(mySong.paused) {
+        mySong.play();
+        icon.src = "images/pause.png";
+    } else {
+        mySong.pause();
+        icon.src = "images/play.png";
+    }
+}
