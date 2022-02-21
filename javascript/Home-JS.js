1d53fba79bca28acace03749e8a93a34547227c2
Manuel <mgordillo11@outlook.com>
2022-02-20 19:42:10 -0700

    Added HomePage js file, and fixed up home page layout

--- javascript/Home-JS.js
+++ javascript/Home-JS.js
@@ -6,16 +6,19 @@ icon.onclick = async function() {
     const image2 = async function image() {
         return await fetch('https://picsum.photos/2000/1000')
     }
-    /*const inspritationalQuote = async function quote() {
-        return await fetch('https://inspiration.goprogram.ai')
-    }*/
+
+    const inspritationalQuote = async function quote() {
+        return await fetch('https://api.fisenko.net/v1/quotes/en/random')
+    }
     
     const image3 = await image2()
-    //const insQuote = await inspritationalQuote()
+    const insQuote = await inspritationalQuote()
 
 
     const updatedImage = image3.url
+
     document.getElementById("song-pic").src = updatedImage
+    document.getElementById("quote-pic").innerText = "Hello"
 
     console.log(image3.url)
     if(mySong.paused) {
