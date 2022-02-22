bfbb3ad3f4ff984809dac81122f91d7a7929e1c6
DallinVetter <dalljvetter@gmail.com>
2022-02-21 23:11:43 -0700

    fixed weather styling

--- javascript/about.js
+++ javascript/about.js
@@ -37,7 +37,7 @@ document.getElementById("weatherSubmit").addEventListener("click", function (eve
             var timestr = date.toLocaleTimeString();
 
             results += '<p><strong>Sunrise Time:  </strong>' + date + timestr + "</p>"
-            results += '<img src="/images/sunrise.jpeg" width=50%> </div>'
+            //results += '<img src="/images/sunrise.jpeg" width=50%> </div>'
             results += '<div class="sunset-sunrise">'
 
             var sec = json.sys.sunset;
@@ -45,45 +45,7 @@ document.getElementById("weatherSubmit").addEventListener("click", function (eve
             var timestr = date.toLocaleTimeString();
 
             results += '<p><strong>Sunset Time:  </strong>' + date + timestr + "</p>"
-            results += '<img src="/images/sunset.jpeg" width=50%> </div>'
+            //results += '<img src="/images/sunset.jpeg" width=50%> </div>'
             document.getElementById("weatherResults").innerHTML = results;
         }).catch(error => alert("Invalid City. Please enter a valid United States City"))
-
-    const url2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + value + ", US&units=imperial" + "&APPID=d6f98e0b57de008e9dfc77ecc5725d47";
-    fetch(url2)
-        .then(function (response) {
-            return response.json();
-        }).then(function (json) {
-            let forecast = "";
-            forecast += '<h2>5 Day Forecast for ' + json.city.name + ' in 3 Hour Increments</h2>'
-            for (let i = 0; i < json.list.length; i++) {
-                if (i % 2 == 0) {
-                    forecast += "<div class = 'tuple'>"
-                }
-                forecast += "<div class = 'forecast-box'><h4>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</h4>";
-                forecast += '<div class = "stuff"><img class = "icon2" src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>'
-                forecast += "<p class = 'temperature2'><strong>Average Temperature: </strong>" + json.list[i].main.temp
-                forecast += "<br><strong> Will Feel Like: </strong>" + json.list[i].main.feels_like + " &deg;F"
-                forecast += "<br><strong> High: </strong>" + json.list[i].main.temp_max + " &deg;F"
-                forecast += "<br> <strong>Low: </strong>" + json.list[i].main.temp_min + " &deg;F"
-                forecast += "<br><strong> Description: </strong>"
-                for (let i = 0; i < json.list[i].weather.length; i++) {
-                    forecast += json.list[i].weather[i].description
-                    if (i !== json.list[i].weather.length - 1)
-                        forecast += ", "
-                }
-                forecast += "<br><strong>Chance of Rain: </strong>" + (json.list[i].pop * 100) + "%"
-                forecast += "</p><p class = 'temperature2'><strong>Pressure: </strong>" + json.list[i].main.pressure + " hPa"
-                forecast += "<br><strong>Humidity: </strong>" + json.list[i].main.humidity + "%"
-                forecast += "<br><strong>Visibility: </strong>" + json.list[i].visibility + " meters"
-                forecast += "<br><strong>Wind Speed: </strong>" + json.list[i].wind.speed + " mph"
-                forecast += "<br><strong>Wind Degree: </strong>" + json.list[i].wind.deg + "&deg;"
-                forecast += "<br><strong>Cloudiness Percent: </strong>" + json.list[i].clouds.all + " %"
-                forecast += "</p></div></div>";
-                if (i % 2 == 1) {
-                    forecast += "</div>"
-                }
-            }
-            document.getElementById("forecastResults").innerHTML = forecast;
-        });
 });
